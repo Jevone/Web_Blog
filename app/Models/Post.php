c790d8f2f78e76122040b8f5e3395a5aa8b79245
Jefri Jaya <jefrijaya08@gmail.com>
2024-09-25 19:49:56 +0700

    Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,40 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Post{
-    public static function all(){
-        return [
-            [   
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Jefri Jaya',
-                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia fugit dicta numquam, asperiores voluptatum fuga ex earum, architecto, qui doloremque quia mollitia maiores exercitationem nostrum.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Acikiwirkurlala',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa facilis et eaque cumque. Quis ipsum at minus quibusdam debitis tenetur dolore soluta explicabo. Perspiciatis, qui.'
-            ]
-        ];
-    }
-
-    public static function find($slug): array 
-    {
-        // return Arr::first(static::all(), function($post) use ($slug){
-        //     return $post['slug'] == $slug;
-        // });
-
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug );
-
-        if(!$post){
-            abort(404);
-        }
-
-        return $post;
-    }
-
+class Post extends Model {
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,10 +5,10 @@
 
     <article class="py-8 max-w-screen-md border-b border-grey-300">
         <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
-            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }} </h2>
         </a>
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 1 January 2024
+            <a href="#">{{ $post->author }}</a> | {{ $post->created_at->diffForHumans() }}
         </div>
         <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
         <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
